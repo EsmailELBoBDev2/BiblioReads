7ab45bbdad65c5ddd3861fd5914b10122242b079
Nesaku <78386413+nesaku@users.noreply.github.com>
2022-12-17 19:04:30 -0500

    Delete pages directory

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-import Error from "next/error";
-
-export default function Custom404() {
-  return (
-    <div>
-      <Error statusCode={404} title={"This page could not be found"} />
-    </div>
-  );
-}
--- pages/500.js
+++
@@ -1,9 +0,0 @@
-import Error from "next/error";
-
-export default function Custom500() {
-  return (
-    <div>
-      <Error statusCode={500} title={"Internal Server Error"} />
-    </div>
-  );
-}
